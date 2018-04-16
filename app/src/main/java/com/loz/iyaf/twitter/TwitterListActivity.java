103703fc86ab67efa1db8fdf42e2d2f2c59002b3
Loz Archer <loz@the-pub.org>
2018-04-16 14:42:01 +0100

    Running Twitter view on thread

--- app/src/main/java/com/loz/iyaf/twitter/TwitterListActivity.java
+++ app/src/main/java/com/loz/iyaf/twitter/TwitterListActivity.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.loz.R;
+import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
@@ -25,25 +26,24 @@ public class TwitterListActivity extends AppCompatActivity {
         }
         setTitle(hashtag+" Tweets");
         String searchTerm = getString(R.string.twitter_searchterm);
-        //String searchTerm = "asdasdsadsadasdsad";
 
-        final SearchTimeline searchTimeLine = new SearchTimeline.Builder()
-                .query(searchTerm)
-                .maxItemsPerRequest(20)
-                .resultType(SearchTimeline.ResultType.RECENT)
-                .build();
+        new Thread(() -> {
+            final SearchTimeline searchTimeLine = new SearchTimeline.Builder()
+                    .query(searchTerm)
+                    .maxItemsPerRequest(20)
+                    .resultType(SearchTimeline.ResultType.RECENT)
+                    .build();
+            final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
+                    .setTimeline(searchTimeLine)
+                    .build();
+            showUi(adapter);
+        }).start();
+    }
 
-        final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
-                .setTimeline(searchTimeLine)
-                .build();
-
-
+    private void showUi(TweetTimelineListAdapter adapter) {
         ListView listView = findViewById(android.R.id.list);
         listView.setAdapter(adapter);
-
         TextView emptyView = findViewById(android.R.id.empty);
         listView.setEmptyView(emptyView);
     }
-
-
 }
