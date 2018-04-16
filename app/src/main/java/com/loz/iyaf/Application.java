04b976f5cee681966d974d93cab9dc8a7b2da51b
Loz Archer <loz@the-pub.org>
2018-04-16 10:47:17 +0100

    Initialising Twitter on thread on application startup

--- app/src/main/java/com/loz/iyaf/Application.java
+++ app/src/main/java/com/loz/iyaf/Application.java
@@ -1,5 +1,7 @@
 package com.loz.iyaf;
 
+import com.twitter.sdk.android.core.Twitter;
+
 /**
  * Created by loz on 27/03/2018.
  */
@@ -9,5 +11,6 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        new Thread(() -> Twitter.initialize(this)).start();
     }
 }
--- app/src/main/java/com/loz/iyaf/twitter/TwitterListActivity.java
+++ app/src/main/java/com/loz/iyaf/twitter/TwitterListActivity.java
@@ -2,10 +2,17 @@ package com.loz.iyaf.twitter;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.loz.R;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
 import com.twitter.sdk.android.tweetui.SearchTimeline;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 
@@ -14,7 +21,6 @@ public class TwitterListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Twitter.initialize(this);
 
         setContentView(R.layout.activity_twitterlist);
 
@@ -24,16 +30,25 @@ public class TwitterListActivity extends AppCompatActivity {
         }
         setTitle(hashtag+" Tweets");
         String searchTerm = getString(R.string.twitter_searchterm);
+        //String searchTerm = "asdasdsadsadasdsad";
+
         final SearchTimeline searchTimeLine = new SearchTimeline.Builder()
                 .query(searchTerm)
+                .maxItemsPerRequest(20)
+                .resultType(SearchTimeline.ResultType.RECENT)
                 .build();
 
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(this)
                 .setTimeline(searchTimeLine)
                 .build();
 
-        ListView listView = (ListView) findViewById(android.R.id.list);
+
+        ListView listView = findViewById(android.R.id.list);
         listView.setAdapter(adapter);
+
+        TextView emptyView = findViewById(android.R.id.empty);
+        listView.setEmptyView(emptyView);
     }
+
 
 }
